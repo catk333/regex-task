7bd8bf346abc13d702b1d198cc10860bceda018a
Catherine Waind <catherine.waind@bbc.co.uk>
2020-06-25 14:04:58 +0100

    Reg ex Task 1 completed

---
+++ regEx.js
@@ -0,0 +1,23 @@
+
+var fs = require('fs');
+
+fs.readFile('DataForRegExTask','utf8',function (err,data){
+
+
+let stringOfData = data.toString();
+
+
+let splitOnSpaces = stringOfData.split(" ");
+  
+let softWireArray = splitOnSpaces.filter(item => item.includes("@softwire.com"));
+
+console.log(softWireArray.length);
+
+})
+
+
+
+
+
+
+
